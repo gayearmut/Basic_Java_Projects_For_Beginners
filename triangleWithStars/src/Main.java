1b44804e885c92af7b31dacefb7b30beab3a4384
Gaye Armut <57320188+gayearmut@users.noreply.github.com>
2022-12-04 20:59:53 +0300

    Add files via upload

---
+++ triangleWithStars/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i =0; i<n; i++){
+
+            for(int j =n;j > i; j--){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <=i ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
